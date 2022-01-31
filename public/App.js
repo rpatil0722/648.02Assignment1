39f8b73ae46cd32e9faf0791617e59df9e6feacb
rpatil0722 <90164861+rpatil0722@users.noreply.github.com>
2022-01-30 20:30:55 -0800

    commit to main

---
+++ public/App.js
@@ -0,0 +1,32 @@
+const NAME = "Raj Patil";
+
+const element = React.createElement(
+	"div",
+	{ className: "container border" },
+	React.createElement(
+		"h1",
+		{ className: "border" },
+		NAME
+	),
+	React.createElement("img", { className: "border", src: "https://static-beautyhigh.stylecaster.com/2014/05/477973067.jpg", width: "300px" }),
+	React.createElement(
+		"p",
+		null,
+		"A Fullstack Web Developer from San Diego,California. International Graduate Student at San Diego State University pursuing Masters in Computer Science with an expected graduation date of May 2023. Actively seeking internship opportunities in Software Engineering for Summer 2022."
+	),
+	React.createElement(
+		"a",
+		{ href: "https://github.com/rpatil0722", target: "_blank" },
+		React.createElement(
+			"button",
+			{ className: "border" },
+			React.createElement(
+				"h3",
+				null,
+				"VIEW MY GITHUB REPO"
+			)
+		)
+	)
+);
+
+ReactDOM.render(element, document.getElementById('root'));
